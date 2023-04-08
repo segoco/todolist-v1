4f69ff0273b54e11061f637e4cc33910f0889614
Sergio Gomez C <segoco@gmail.com>
2023-04-07 22:11:40 -0500

    list items added dynamically

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 /* eslint-disable quotes */
+let items = [];
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -6,13 +8,24 @@ const app = express();
 
 // tell app to use ejs as the view engine
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const today = new Date();
 const day = today.toLocaleDateString("en-US", { weekday: "long", month: "long", day: "numeric" });
 
-// route to home page
+// route to home page (get request)
 app.get("/", (req, res) => {
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, itemList: items });
+  console.log(items);
+});
+
+// route to home page (post request)
+app.post("/", (req, res) => {
+  // get the value of the input field
+  const item = req.body.newItem;
+  console.log(item);
+  items.push(item);
+  res.redirect("/");
 });
 
 // listen to port 3000
