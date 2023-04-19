e106793d01f4a024b4e5a643863b1d97bedaa776
Sergio Gomez C <segoco@gmail.com>
2023-04-19 15:39:02 -0500

    Adjustments to initial version

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const workItems = [];
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const date = require(__dirname + "/date.js");
+const day = date.getDate();
+
 const app = express();
 
 // tell app to use ejs as the view engine
@@ -12,15 +15,15 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const today = new Date();
-const day = today.toLocaleDateString("en-US", { weekday: "long", month: "long", day: "numeric" });
-
 // route to home page (get request)
 app.get("/", (req, res) => {
   res.render("list", { listName: day, itemList: items });
 });
 app.get("/work", (req, res) => {
   res.render("list", { listName: "Work", itemList: workItems });
+});
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 // route to home page (post request)
